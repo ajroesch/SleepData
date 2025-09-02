16829e9f7d03a8e22dfdca6b95fd4e1bb045a553
ajroesch <aroesch@my.wctc.edu>
2025-09-02 14:04:09 -0500

    [1.05] Generate sleep data

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ string? resp = Console.ReadLine();
 if (resp == "1")
 {
     // Create data file
+
     //ask how many weeks
     Console.WriteLine("How many weeks of data do you want to enter?");
     //input the response (convert to int)
@@ -17,8 +18,25 @@ if (resp == "1")
     DateTime today = DateTime.Now;
     //we want full weeks sunday - saturday
     DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-    //subtract number of weeks from dataEndDate to get dataStartDate
-    DateTime dataStartDate = dataEndDate.AddDays(-((weeks * 7)));
+    //subtract number of weeks from dataEndDate to get dataDate
+    DateTime dataDate = dataEndDate.AddDays(-((weeks * 7)));
+
+    Random rand = new();
+
+    while (dataDate < dataEndDate)
+    {
+        int[] hours = new int[7];
+        for (int i = 0; i < hours.Length; i++)
+        {
+            hours[i] = rand.Next(4, 13);
+        }
+
+        //write data to text file
+        //M/d/yy,#|#|#|#|#|#|#
+        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+
+        dataDate = dataDate.AddDays(7);
+    }
 
 }
 else if (resp == "2")
